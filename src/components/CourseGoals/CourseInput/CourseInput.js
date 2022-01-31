6bb554fd3be57448ac0e2cf6f477a5af6fe6f29e
PavelRnD <98314322+PavelRnD@users.noreply.github.com>
2022-01-31 15:59:40 +0300

    validation

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -7,17 +7,26 @@ const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length>0){
+      setValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
+  const [isValid, setValid] = useState(true);
+
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length===0){
+      setValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid? 'invalid': ''}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
